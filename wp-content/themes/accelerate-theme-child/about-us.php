a8f86ba0e0c1830a31570d6373b6c255be062360
Sinon89SAO <alexandra5.12.08@gmail.com>
2017-04-07 07:25:27 -0600

    About page changes

---
+++ wp-content/themes/accelerate-theme-child/about-us.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template name: Custom About Page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Theme
+ * @since Accelerate Theme 1.1
+ */
+
+get_header(); ?>
+   <section class="home-page">
+   <div class="site-content">
+
+    <?php while ( have_posts() ) : the_post();?>
+      <h2 class="about-hero"> <span>Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</h2>
+  </div>
+  </section>
+  <?php endwhile; // end of the loop. ?>
+  <section class="our-services">
+    <div class="site-content">
+
+      <h4>Our Services</h4>
+      <p>We take pride in our clients and the content we create for them.<br>
+        Here's a brief overview of our offered services</p>
+ </div>
+ </section>
+
+  <?php query_posts('post_type=services');
+
+      while ( have_posts() ) : the_post();
+
+      $service_title = get_field('service_title');
+
+      $service_description= get_field('service_description');
+
+      $service_image = get_field('service_image');
+
+      $size = "full";
+      ?>
+
+      <div class="row">
+      <div class="col-lg-8" id="service-desc">
+      		<h2><?php echo $service_title; ?></h2>
+      		<p><?php the_content(); ?> </p>
+      </div>
+
+
+     <div class="service-image">
+        <?php if($service_image) {
+           echo wp_get_attachment_image ($service_image, $size); ?>
+        <?php } ?>
+      </div>
+<?php endwhile; // end of the loop. ?>
+
+  <?php wp_reset_query(); ?>
+
+  <section>
+
+<ul class="contact-button">
+
+	<li><h3>Interested in working with us?</h3></li>
+	<li><a class="button" href="#">Contact Us</a></li>
+	</ul>
+</section>
+
+
+<?php get_footer(); ?>
